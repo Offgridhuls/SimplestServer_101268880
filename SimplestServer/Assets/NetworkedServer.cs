7301c37482b2b01e821a909bd78a452873758e30
Offgridhuls <55862297+Offgridhuls@users.noreply.github.com>
2021-12-16 17:53:46 -0500

    - Added server support for streaming data lab
    - Client to server signifiers:
    JoinSharingRoom
    PartyTransferDataStart
    PartyTransferData
    PartyTransferDataEnd
    
    - Added server side support to save character data from streaming data lab

--- SimplestServer/Assets/NetworkedServer.cs
+++ SimplestServer/Assets/NetworkedServer.cs
@@ -22,6 +22,8 @@ public class NetworkedServer : MonoBehaviour
 
     public PlayerAccount winnerAccount;
 
+    LinkedList<SharingRoom> sharingRooms;
+
     Match newMatch = new Match();
     // Start is called before the first frame update
 
@@ -34,6 +36,8 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
+
+        sharingRooms = new LinkedList<SharingRoom>();
 
 
     }
@@ -69,6 +73,7 @@ public class NetworkedServer : MonoBehaviour
                 {
                     newMatch.connectedAccounts.Remove(player);
                 }
+                PlayerDisconnected(recConnectionID);
                 Debug.Log("Disconnection, " + recConnectionID);
                 break;
         }
@@ -87,10 +92,14 @@ public class NetworkedServer : MonoBehaviour
     IEnumerator SendReplay(List<Plays> plays, int id)
     {
         Debug.Log(plays.Count);
+
         for (int i = 0; i < plays.Count; i++)
         {
+
             SendMessageToClient(ServerToClientSignifiers.playTurn + "," + (plays[i].isO ? "1" : "0" ) + "," + plays[i].index + ",0", id);
+
             yield return new WaitForSeconds(.2f);
+
         }
         SendMessageToClient(ServerToClientSignifiers.winner + ",0," + winnerAccount.name, id);
     }
@@ -168,8 +177,10 @@ public class NetworkedServer : MonoBehaviour
         }
         else if(signifier == ClientToServerSignifiers.sendPlay)
         {
+
             if(int.TryParse(csv[1], out var result))
             {
+
                 var player = newMatch.connectedAccounts.FirstOrDefault(x => x.connectionID == id);
                 if(player != null)
                 {
@@ -223,9 +234,127 @@ public class NetworkedServer : MonoBehaviour
         {
             StartCoroutine(SendReplay(playsList, id));
         }
+        if(signifier == ClientToServerSignifiers.JoinSharingRoom)
+        {
+            string name = csv[1];
+
+            bool roomExists = false;
+
+            foreach(SharingRoom sr in sharingRooms)
+            {
+                if(sr.name == name)
+                {
+                    roomExists = true;
+                    sr.playerConnectionIDs.AddLast(id);
+                    Debug.Log("Created");
+                }
+            }
+            if(!roomExists)
+            {
+                SharingRoom sr = new SharingRoom();
+                sr.name = name;
+                sr.playerConnectionIDs.AddLast(id);
+                sharingRooms.AddLast(sr);
+                Debug.Log("joined");
+            }
+        }
+        if(signifier == ClientToServerSignifiers.PartyTransferDataStart)
+        {
+            SharingRoom sr = GetSharingRoomWithPlayerID(id);
+            sr.sharingPartyData = new LinkedList<string>();
+        }
+
+        if (signifier == ClientToServerSignifiers.PartyTransferData)
+        {
+            SharingRoom sr = GetSharingRoomWithPlayerID(id);
+
+            sr.sharingPartyData.AddLast(msg);
+        }
+
+        if (signifier == ClientToServerSignifiers.PartyTransferDataEnd)
+        {
+            SharingRoom sr = GetSharingRoomWithPlayerID(id);
+
+            foreach(int playerID in sr.playerConnectionIDs)
+            {
+                if (id == playerID)
+                    continue;
+
+                SendMessageToClient(ServerToClientSignifiers.PartyTransferDataStart + "", playerID);
+
+                foreach(string d in sr.sharingPartyData)
+                {
+                    SendMessageToClient(d, playerID);
+                }
+
+                SendMessageToClient(ServerToClientSignifiers.PartyTransferDataEnd + "", playerID);
+
+            }
+        }
+
+    }
+    public void PlayerDisconnected(int id)
+    {
+
+        SharingRoom sharingRoomWithPlayerID = GetSharingRoomWithPlayerID(id);
+
+
+        foreach(SharingRoom sr in sharingRooms)
+        {
+            foreach(int playerID in sr.playerConnectionIDs)
+            {
+                if(playerID == id)
+                {
+                    sharingRoomWithPlayerID = sr;
+                    
+                }
+            }
+           
+        }
+        if(sharingRoomWithPlayerID != null)
+        {
+            sharingRoomWithPlayerID.playerConnectionIDs.Remove(id);
+            if(sharingRoomWithPlayerID.playerConnectionIDs.Count <= 0)
+            {
+                sharingRooms.Remove(sharingRoomWithPlayerID);
+            }
+
+        }
+    }
+    private SharingRoom GetSharingRoomWithPlayerID(int id)
+    {
+        foreach (SharingRoom sr in sharingRooms)
+        {
+            foreach (int playerID in sr.playerConnectionIDs)
+            {
+                if (playerID == id)
+                {
+                    return sr;
+
+                    SendMessageToClient(ServerToClientSignifiers.PartyTransferDataStart + "", playerID);
+
+                }
+            }
+
+        }
+
+        return null;
     }
 }
 
+public class SharingRoom
+{
+    public LinkedList<int> playerConnectionIDs;
+
+    public string name;
+
+    public LinkedList<string> sharingPartyData;
+
+    public SharingRoom()
+    {
+        playerConnectionIDs = new LinkedList<int>();
+    }
+}
 
 public class PlayerAccount
 {
@@ -252,6 +381,14 @@ public static class ClientToServerSignifiers
     public const int sendPlay = 3;
 
     public const int sendReplay = 4;
+
+    public const int JoinSharingRoom = 5;
+
+    public const int PartyTransferDataStart = 100;
+
+    public const int PartyTransferData = 101;
+
+    public const int PartyTransferDataEnd = 102;
 }
 
 public static class ServerToClientSignifiers
@@ -271,5 +408,11 @@ public static class ServerToClientSignifiers
     public const int playTurn = 7;
 
     public const int winner = 8;
+
+    public const int PartyTransferDataStart = 100;
+
+    public const int PartyTransferData = 101;
+
+    public const int PartyTransferDataEnd = 102;
 
 }
--- SimplestServer/Assets/Plays.cs
+++ SimplestServer/Assets/Plays.cs
@@ -8,7 +8,6 @@ public class Plays
     public int index;
     public bool isO;
  
-
     public void SetListValues(int index, bool isO)
     {
         this.index = index;
